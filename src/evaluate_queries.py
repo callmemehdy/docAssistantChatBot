21b4b288907e00a7fa67d3e6550d88edf5bb361c
callmemehdy <rock.mido15@gmail.com>
2025-10-07 18:26:46 +0100

    make some fixes on the query similarities

--- src/evaluate_queries.py
+++ src/evaluate_queries.py
@@ -10,7 +10,7 @@ from embd import get_embedding_function
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+Answer the question based only on the following context (dont include anything from the prompt i gave you in the response):
 
 {context}
 
@@ -42,20 +42,17 @@ def main():
 def query_rag(query_text: str):
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-
-    results = db.similarity_search_with_score(query_text, k=2)
-
-    # for doc, _ in results:
-    #     print(f"{doc}\n-----------------------------------------------------------------------\n", file=sys.stderr)
+   
+    results = db.similarity_search_with_score(query_text, k=5)
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+   
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-
     model = OllamaLLM(model="mistral")
+   
     response_text = model.invoke(prompt)
-
     sources = [doc.metadata.get("id", None) for doc, _score in results]
-
+   
     return response_text, sources
 
 if __name__ == "__main__":
